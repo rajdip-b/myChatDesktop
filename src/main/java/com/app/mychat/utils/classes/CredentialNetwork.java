02e49d4f3225043ee6555681540764fd5dde8820
rajdip-b <agentR47@gmail.com>
2021-09-26 21:08:53 +0530

    Re-created ChatScreen fxml file and fragments

--- src/main/java/com/app/mychat/utils/classes/CredentialNetwork.java
+++ src/main/java/com/app/mychat/utils/classes/CredentialNetwork.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 public class CredentialNetwork {
 
     private final CredentialNetworkListener networkListener;
-    //	private static final String serverIP = "3.109.181.96";
+//    	private static final String serverIP = "3.109.181.96";
     private static final String serverIP = "127.0.0.1";
     private static final int credentialServerPort = 5000;
     private ObjectOutputStream objectOutputStream;
