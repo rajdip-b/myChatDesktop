5b6d03644daaab8986767ab285256de1b90a254f
Ankitasen21 <senankita894@gmail.com>
2021-09-26 18:15:03 +0530

    added chatScreen

---
+++ src/main/java/com/app/mychat/controller/chatScreenController.java
@@ -0,0 +1,15 @@
+package com.app.mychat.controller;
+
+import javafx.event.ActionEvent;
+import com.app.mychat.utils.classes.Animations;
+import com.app.mychat.utils.classes.CredentialNetwork;
+import com.app.mychat.utils.classes.KeyValues;
+import com.app.mychat.utils.interfaces.CredentialNetworkListener;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class chatScreenController {
+    public void clickbutton(ActionEvent actionEvent) {
+    }
+}
