18634ec9dcf20cf4fb75832a0bdafcd9f1b36bd2
rajdip-b <agentR47@gmail.com>
2021-10-06 13:35:14 +0530

    Created basic update functionality

--- src/main/java/com/app/mychat/utils/classes/update/GitHub.java
+++ src/main/java/com/app/mychat/utils/classes/update/GitHub.java
@@ -1,4 +1,15 @@
 package com.app.mychat.utils.classes.update;
 
 public class GitHub {
+
+    private String githubReleaseLink;
+
+    public GitHub(String versionID){
+        githubReleaseLink = "https://github.com/rajdip-b/myChatDesktop/releases/tag/" + versionID;
+    }
+
+    public String getLink(){
+        return githubReleaseLink;
+    }
+
 }
--- src/main/java/com/app/mychat/utils/classes/update/Update.java
+++ src/main/java/com/app/mychat/utils/classes/update/Update.java
@@ -1,4 +1,9 @@
 package com.app.mychat.utils.classes.update;
 
 public class Update {
+
+    public static void main(String[] args) {
+        new UpdateChecker(new GitHub("v2.0.0")).checkForVersion();
+    }
+
 }
