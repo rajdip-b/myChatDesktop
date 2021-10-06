1a2fb14ecb71a97f174a08ab35376bd7a5678e98
rajdip-b <agentR47@gmail.com>
2021-10-06 13:35:14 +0530

    Created basic update functionality

--- src/main/java/com/app/mychat/utils/classes/update/GitHub.java
+++ src/main/java/com/app/mychat/utils/classes/update/GitHub.java
@@ -1,4 +1,15 @@
 package com.app.mychat.utils.classes.update;
 
 public class GitHub {
+
+    private String githubReleaseLink;
+
+    public GitHub(String versionID){
+        githubReleaseLink = "https://github.com/rajdip-b/myChatDesktop/releases/myChatDesktop_" + versionID;
+    }
+
+    public String getLink(){
+        return githubReleaseLink;
+    }
+
 }
--- src/main/java/com/app/mychat/utils/classes/update/UpdateChecker.java
+++ src/main/java/com/app/mychat/utils/classes/update/UpdateChecker.java
@@ -1,4 +1,29 @@
 package com.app.mychat.utils.classes.update;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class UpdateChecker {
+
+    private GitHub gitHub;
+
+    public UpdateChecker(GitHub gitHub){
+        this.gitHub = gitHub;
+    }
+
+    public void checkForVersion(){
+        try{
+            URL url = new URL(gitHub.getLink());
+            HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+            if (httpsURLConnection.getResponseCode() == 200)
+                System.out.println("Version available");
+            else
+                System.out.println("Failure!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
